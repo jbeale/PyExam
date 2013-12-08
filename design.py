5ff538b6465f6fb6d93b511d8e869def3cfcda62
Justin Beale <jbeale@mifhjbeale-m2.hsd1.mi.comcast.net>
2013-12-08 10:54:01 -0500

    initial commit

---
+++ design.py
@@ -0,0 +1,16 @@
+def getItem(id):
+    pass
+    #will get an individual test question from the database
+
+def saveItem(item):
+    pass
+    #will save a new question and return its integer ID in the database
+
+def getActivity(id):
+    pass
+    #will get an entire activity and references to the ids of all its questions
+
+def saveActivity(activity):
+    pass
+    #will create a new activity in the database and return its integer ID
+
